15304dfbf27d10ee17fee9c1240b5ab5a88d4b1b
danieldwipaska <gniel6487@gmail.com>
2022-10-13 07:11:34 +0700

    menghilangkan auth dan dashboard

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ const path = require('path');
 // const indexRoute = require('./routes/index.js');
 // const qrcode = require('qrcode-terminal');
 const QRCode = require('qrcode');
-const { Client, MessageMedia, Buttons, LocalAuth, List } = require('whatsapp-web.js');
+const { Client, MessageMedia, Buttons } = require('whatsapp-web.js');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
-const cookieParser = require('cookie-parser');
+// const cookieParser = require('cookie-parser');
 dotenv.config();
 
-const authRoute = require('./routes/auth');
+// const authRoute = require('./routes/auth');
 const indexRoute = require('./routes/index');
 
 const client = new Client({
@@ -32,7 +32,7 @@ app.use(express.urlencoded({ limit: '10mb', extended: false }));
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(`${__dirname}/public`)); // make files able to access
 app.use('/public', express.static(path.join(__dirname, 'public')));
-app.use(cookieParser());
+// app.use(cookieParser());
 
 // Mongodb Connect
 mongoose
@@ -57,7 +57,7 @@ client.on('qr', (qr) => {
 
 //ROUTES
 app.use('/', indexRoute);
-app.use('/api/auth', authRoute);
+// app.use('/api/auth', authRoute);
 
 app.get('/api/qr', (req, res) => {
   res.json({
@@ -72,43 +72,63 @@ client.on('ready', () => {
 });
 
 client.on('message', async (message) => {
-  const tanggal = '00/00/00';
-  const periode = '00/00/00 - 00/00/00';
-  const buttons_reply = new Buttons(
-    `Ini adalah Layanan Whatsapp GPIB Immanuel Malang.\nAnda bisa mendapatkan dokumen Warta Jemaat dan Jadwal Ibadah PELKAT dengan menekan tombol di bawah.\nLayanan ini tersedia 24 jam.\n\n_Dokumen terbaru:_\n_Warta Jemaat: ${tanggal}_\n_Jadwal Ibadah: ${periode}_`,
-    [
-      { body: 'Warta Jemaat', id: 'test-1' },
-      { body: 'Jadwal Ibadah', id: 'test-2' },
-    ],
-    'SELAMAT DATANG!!!',
-    'Pilih dokumen yang anda inginkan'
-  ); // Reply button
-
-  const buttons_reply_lainlain = new Buttons('GPIB Immanuel Malang juga dapat memberi anda dokumen-dokumen lainnya, seperti:', [
-    { body: 'Litbang', id: 'test-4' },
-    { body: 'Info', id: 'test-5' },
-    { body: 'Merchandise', id: 'test-6' },
-  ]); // Reply button
-
-  const buttons_reply_mulai = new Buttons('Layanan ini sudah berakhir atau belum dimulai.', [{ body: 'Mulai', id: 'test-7' }], 'Layanan Whatsapp GPIB Immanuel Malang', 'Silahkan tekan tombol di bawah untuk memulai.'); // Reply button
-
-  const media = MessageMedia.fromFilePath('./public/pdf/warta_jemaat_minggu_tanggal.pdf');
-
-  if (message.body === 'Warta Jemaat') {
-    await client.sendMessage(message.from, media);
-  } else if (message.body === 'Jadwal Ibadah') {
-    await client.sendMessage(message.from, 'Jadwal Ibadah belum ada.');
-  } else if (message.body === 'Litbang') {
-    await client.sendMessage(message.from, 'Litbang belum ada.');
-  } else if (message.body === 'Info') {
-    await client.sendMessage(message.from, 'Info belum ada.');
-  } else if (message.body === 'Merchandise') {
-    await client.sendMessage(message.from, 'Merchandise belum ada.');
-  } else if (message.body === 'Mulai') {
-    await client.sendMessage(message.from, buttons_reply);
-    await client.sendMessage(message.from, buttons_reply_lainlain);
+  if (message.from === '6285172160300@c.us') {
+    if (message.hasMedia) {
+      const mediaReceived = await message.downloadMedia();
+      if (mediaReceived.filename === 'warta_jemaat.pdf') {
+        //SIMPAN WARTA JEMAAT KE DB
+        await client.sendMessage(message.from, 'warta jemaat diterima');
+      } else if (mediaReceived.filename === 'tata_ibadah.pdf') {
+        //SIMPAN TATA IBADAH KE DB
+        await client.sendMessage(message.from, 'tata ibadah diterima');
+      } else if (mediaReceived.filename === 'undefined') {
+        //SIMPAN JADWAL IBADAH KE DB
+        await client.sendMessage(message.from, 'jadwal ibadah diterima');
+      } else {
+        await client.sendMessage(message.from, 'file tidak dikenal');
+      }
+    } else {
+      await client.sendMessage(message.from, 'Hai haloo');
+    }
   } else {
-    await client.sendMessage(message.from, buttons_reply_mulai);
+    const tanggal = '00/00/00';
+    const periode = '00/00/00 - 00/00/00';
+    const buttons_reply = new Buttons(
+      `Ini adalah Layanan Whatsapp GPIB Immanuel Malang.\nAnda bisa mendapatkan dokumen Warta Jemaat dan Jadwal Ibadah PELKAT dengan menekan tombol di bawah.\nLayanan ini tersedia 24 jam.\n\n_Dokumen terbaru:_\n_Warta Jemaat: ${tanggal}_\n_Jadwal Ibadah: ${periode}_`,
+      [
+        { body: 'Warta Jemaat', id: 'test-1' },
+        { body: 'Jadwal Ibadah', id: 'test-2' },
+      ],
+      'SELAMAT DATANG!!!',
+      'Pilih dokumen yang anda inginkan'
+    ); // Reply button
+
+    const buttons_reply_lainlain = new Buttons('GPIB Immanuel Malang juga dapat memberi anda dokumen-dokumen lainnya, seperti:', [
+      { body: 'Litbang', id: 'test-4' },
+      { body: 'Info', id: 'test-5' },
+      { body: 'Merchandise', id: 'test-6' },
+    ]); // Reply button
+
+    const buttons_reply_mulai = new Buttons('Layanan ini sudah berakhir atau belum dimulai.', [{ body: 'Mulai', id: 'test-7' }], 'Layanan Whatsapp GPIB Immanuel Malang', 'Silahkan tekan tombol di bawah untuk memulai.'); // Reply button
+
+    // const media = MessageMedia.fromFilePath('./public/pdf/warta_jemaat_minggu_tanggal.pdf');
+
+    if (message.body === 'Warta Jemaat') {
+      await client.sendMessage(message.from, 'Warta Jemaat belum ada');
+    } else if (message.body === 'Jadwal Ibadah') {
+      await client.sendMessage(message.from, 'Jadwal Ibadah belum ada.');
+    } else if (message.body === 'Litbang') {
+      await client.sendMessage(message.from, 'Litbang belum ada.');
+    } else if (message.body === 'Info') {
+      await client.sendMessage(message.from, 'Info belum ada.');
+    } else if (message.body === 'Merchandise') {
+      await client.sendMessage(message.from, 'Merchandise belum ada.');
+    } else if (message.body === 'Mulai') {
+      await client.sendMessage(message.from, buttons_reply);
+      await client.sendMessage(message.from, buttons_reply_lainlain);
+    } else {
+      await client.sendMessage(message.from, buttons_reply_mulai);
+    }
   }
 });
 
--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,26 @@
 const router = require('express').Router();
-const verify = require('./verifyToken');
+// const verify = require('./verifyToken');
 
-router.get('/', verify, (req, res) => {
-  res.render('index', {
-    layout: 'layouts/main-layout',
-    message: {
-      statusCode: 200,
-    },
-    user: req.validUser.name,
-  });
-});
+// router.get('/', verify, (req, res) => {
+//   res.render('index', {
+//     layout: 'layouts/main-layout',
+//     message: {
+//       statusCode: 200,
+//     },
+//     user: req.validUser.name,
+//   });
+// });
 
-router.get('/login', (req, res) => {
-  res.render('login', {
-    layout: 'layouts/main-layout',
-    message: {
-      statusCode: 200,
-    },
-  });
-});
+// router.get('/login', (req, res) => {
+//   res.render('login', {
+//     layout: 'layouts/main-layout',
+//     message: {
+//       statusCode: 200,
+//     },
+//   });
+// });
 
-router.get('/barcode', verify, (req, res) => {
+router.get('/barcode', (req, res) => {
   res.render('qr', {
     layout: 'layouts/main-layout',
     message: {
